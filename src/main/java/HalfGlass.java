7566050c788e6b1e5e039a2a034134b8c999102d
Svetlana Bushmakina <svetik_5073@mail.ru>
2022-10-30 23:46:07 +0400

    HalfGlass

--- src/main/java/HalfGlass.java
+++ src/main/java/HalfGlass.java
@@ -1,13 +1,13 @@
 public class HalfGlass {
     public static void main(String[] args) {
 
-        int volumeGlass = 300;
+        final int volumeGlass = 300;
         System.out.println("Объем стакана: "+ volumeGlass + " милилитров");
 
-        double percentOfWaterGlass = 0.5;
+        final double percentOfWaterGlass = 0.5;
         System.out.println("Cтакан заполнен на " + percentOfWaterGlass * 100 + " %");
 
-        double volumeWaterOfGlass = volumeGlass * percentOfWaterGlass;
+        final double volumeWaterOfGlass = volumeGlass * percentOfWaterGlass;
         System.out.println(percentOfWaterGlass * 100 + " % - это " + volumeWaterOfGlass + " милилитров");
     }
 }
